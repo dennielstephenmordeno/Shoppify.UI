a332a7801a0e296cc969fa99550413dd7b2b281c
Stephen Mordeno <dennielstephen@gmail.com>
2023-06-08 11:30:40 +0800

    fixed clickableCard component

--- src/components/common/ClickableCard.tsx
+++ src/components/common/ClickableCard.tsx
@@ -1,44 +1,81 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Card, CardContent, Typography } from '@mui/material';
+
 interface Props {
   id: number;
   name: string;
   color: string;
   price: number;
   imageUrl: string;
+  hoverImageUrl: string;
   topSeller?: boolean;
   size?: 'small' | 'medium' | 'large' | 'xl';
 }
 
-const ClickableCard: React.FC<Props> = ({ id, name, color, price, imageUrl, topSeller = false, size = 'medium' }) => {
+const ClickableCard: React.FC<Props> = ({
+  id,
+  name,
+  color,
+  price,
+  imageUrl,
+  hoverImageUrl,
+  topSeller = false,
+  size = 'large',
+}) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
     navigate(`/product/${id}`);
   };
 
+  const cardStyle = {
+    backgroundImage: `url(${imageUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    borderRadius: '10px',
+    width: '250px',
+    height: '350px',
+  };
+
+  const cardHoverStyle = {
+    backgroundImage: `url(${hoverImageUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    borderRadius: '10px',
+    width: '250px',
+    height: '350px',
+  };
+
+  const cardContentClassName = `bg-gray-100/50 p-3 ${
+    size === 'small' ? 'w-40' : size === 'medium' ? 'w-64' : size === 'large' ? 'w-80' : 'w-96'
+  }`;
+
   return (
     <Card
       className="cursor-pointer transform hover:scale-105 transition duration-300 ease-in-out"
       onClick={handleClick}
+      style={cardStyle}
+      elevation={3}
     >
-      <CardMedia
-        component="img"
-        height="200"
-        image={imageUrl}
-        alt={name}
-        className="object-cover"
-      />
       {topSeller && (
         <Typography
           variant="subtitle1"
-          className="absolute top-2 left-2 bg-yellow-500 text-white font-bold py-1 px-2 rounded-md"
+          className="absolute top-2 right-2 bg-yellow-500 text-white font-bold py-1 px-2 rounded-md"
         >
           Top Seller
         </Typography>
       )}
-      <CardContent className={`bg-gray-100 p-3 ${size}`}>
+      <CardContent
+        className={cardContentClassName}
+        style={cardStyle}
+        onMouseEnter={(e) => {
+          e.currentTarget.style.backgroundImage = `url(${hoverImageUrl})`;
+        }}
+        onMouseLeave={(e) => {
+          e.currentTarget.style.backgroundImage = `url(${imageUrl})`;
+        }}
+      >
         <Typography variant="h5" component="h2" className="text-lg font-medium">
           {name}
         </Typography>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,43 @@
 import HeroSlider from '../components/common/HeroSlider';
 import ClickableCard from '../components/common/ClickableCard';
-import bgImage1 from '../../assets/heroImage1.jpg';
-import bgImage2 from '../../assets/heroImage2.jpg';
-import bgImage3 from '../../assets/heroImage3.jpg';
+import bgImage1 from '../assets/heroImage1.jpg';
+import bgImage2 from '../assets/heroImage2.jpg';
+import bgImage3 from '../assets/heroImage3.jpg';
+import product1 from '../assets/product1.avif';
+import product1a from '../assets/product2.avif';
 
 
-const products = [
-    {
-      id: 1,
-      name: 'Product 1',
-      color: 'Red',
-      price: 100,
-      imageUrl: '../../assets/heroImage1.jpg',
-      topSeller: true,
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      color: 'Blue',
-      price: 200,
-      imageUrl: '../../assets/heroImage2.jpg',
-    },
-    {
-      id: 3,
-      name: 'Product 3',
-      color: 'Green',
-      price: 300,
-      imageUrl: '../../assets/heroImage3.jpg',
-    },
-  ];
+// const products = [
+//     {
+//       id: 1,
+//       name: 'Product 1',
+//       color: 'Red',
+//       price: 100,
+//       imageUrl: '../assets/heroImage1.jpg',
+//       topSeller: true,
+//     },
+//     {
+//       id: 2,
+//       name: 'Product 2',
+//       color: 'Blue',
+//       price: 200,
+//       imageUrl: '../assets/heroImage2.jpg',
+//     },
+//     {
+//       id: 3,
+//       name: 'Product 3',
+//       color: 'Green',
+//       price: 300,
+//       imageUrl: '../assets/heroImage3.jpg',
+//     },
+//   ];
 
 export const Home = () => {
     return (
         <div>
             <HeroSlider/>
-            <div className='flex justify-center text-center'>
-                {products.map((product) => (
+            <div className="flex justify-center items-center h-screen gap-3">
+                {/* {products.map((product) => (
                     <ClickableCard
                     key={product.id}
                     id={product.id}
@@ -45,7 +47,43 @@ export const Home = () => {
                     imageUrl={product.imageUrl}
                     topSeller={product.topSeller}
                     />
-                ))}
+                ))} */}
+                <ClickableCard
+                  id={2}
+                  name="Product 2"
+                  color="Red"
+                  price={14.99}
+                  imageUrl={bgImage1}
+                  hoverImageUrl={bgImage2}
+                  size="xl"
+                />
+                <ClickableCard
+                  id={2}
+                  name="Product 2"
+                  color="Red"
+                  price={14.99}
+                  imageUrl={bgImage2}
+                  hoverImageUrl={bgImage3}
+                  size="small"
+                />
+                <ClickableCard
+                  id={2}
+                  name="Product 2"
+                  color="Red"
+                  price={14.99}
+                  imageUrl={bgImage1}
+                  hoverImageUrl={bgImage2}
+                  size="large"
+                />
+                <ClickableCard
+                  id={2}
+                  name="Product 2"
+                  color="Red"
+                  price={14.99}
+                  imageUrl={product1}
+                  hoverImageUrl={product1a}
+                  size="medium"
+                />
             </div>
         </div>
     );
